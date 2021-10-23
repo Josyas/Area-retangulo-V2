5e291eda1ff7d440d1a6f33ba60a844d76131282
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-23 17:02:57 -0300

    Add files via upload

---
+++ área de um retangulo/AreaRetangulo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace área_de_um_retangulo {
+    class Retangulo {
+
+       public Retangulo() {
+            this.Altura = 0;
+            this.Base = 0;
+        }
+
+        private int baseret; 
+
+        public int Base {
+            get { return baseret; }
+            set {
+                if (value >= 0) baseret = value;
+                else baseret = 0;
+            }   
+            
+            
+        }
+
+        private int altret;
+
+        public int Altura {
+            get { return altret; }
+            set {
+                if (value >= 0) altret = value;
+                else altret = 0;
+            }
+        }
+
+        public int Area {
+            get { return altret* baseret; }
+        }
+
+        public void ExibeDados() {
+            Console.WriteLine("Base: " + this.Base);
+            Console.WriteLine("Altura: " + this.Altura);
+            Console.WriteLine("Area: " + this.Area);
+        }
+
+        
+    }
+}
---
+++ área de um retangulo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace área_de_um_retangulo {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Calcula a área de um retângulo");
+            Console.Write("Informe a altura: ");
+            retangulo.Altura = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe a base: ");
+            retangulo.Base = Convert.ToInt32(Console.ReadLine());
+            retangulo.ExibeDados();
+            Console.ReadKey();
+        }
+    }
+}
